eb0d8b090e11e6a88480d8a1e33b0b84b01b77ac
Bennett Cyphers <bcyphers@mit.edu>
2017-11-03 01:37:43 -0400

    add some comments to clarify ucb1_bandit; randomly shuffle choices

--- btb/bandit.py
+++ btb/bandit.py
@@ -13,14 +13,24 @@ def ucb1_bandit(arms):
     arms: maps choice IDs to lists of rewards.
         {choice -> list[float]}
     """
-    # total_pulls is capped below at 1, so that log() will not fail
+    # count the total number of times all "levers" have been "pulled" so far.
+    # don't let the value go below 1, so that log() and division still work.
     total_pulls = max(sum(len(r) for r in arms.values()), 1)
     scores = {}
+
+    # shuffle the arms so that if all else is equal, we don't choose the same
+    # one every time
     choices = arms.items()
-    choices.shuffle()
-    for choice, rewards in arms.items():
+    random.shuffle(choices)
+
+    for choice, rewards in choices:
+        # count the number of pulls for this choice, with a floor of 1
         choice_pulls = max(len(rewards), 1)
+
+        # compute the 2-stdev error for the estimate of this choice
         error = math.sqrt(2.0 * math.log(total_pulls) / choice_pulls)
+
+        # this choice's score is the upper bound of what we think is possible
         scores[choice] = np.mean(rewards) + error
 
     best_choice = sorted(scores.keys(), key=scores.get)[-1]
