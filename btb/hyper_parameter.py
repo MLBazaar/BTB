8162b53253663091f86f52befdd9271cb11b1bc2
Bennett Cyphers <bcyphers@mit.edu>
2017-12-12 09:56:58 -0500

    add type checking to hyperparameter initializer

--- btb/hyper_parameter.py
+++ btb/hyper_parameter.py
@@ -18,9 +18,19 @@ CAT_TYPES = [ParamTypes.INT_CAT, ParamTypes.STRING, ParamTypes.BOOL]
 
 # our HyperParameter object
 class HyperParameter(object):
-    def __init__(self, type, range):
-        self.type = type
-        self.range = range
+    def __init__(self, typ, rang):
+        for i in range(len(rang)):
+            if typ in [ParamTypes.INT, ParamTypes.INT_EXP,
+                       ParamTypes.INT_CAT]:
+                rang[i] = int(rang[i])
+            elif typ in [ParamTypes.FLOAT, ParamTypes.FLOAT_EXP]:
+                rang[i] = float(rang[i])
+            elif typ == ParamTypes.STRING:
+                rang[i] = str(rang[i])
+            elif typ == ParamTypes.BOOL:
+                rang[i] = bool(rang[i])
+        self.type = typ
+        self.range = rang
 
     @property
     def is_exponential(self):
