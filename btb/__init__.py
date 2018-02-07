38ffad2bb744cb5c841e6c1b9834d2dcebceb69f
Bennett Cyphers <bcyphers@mit.edu>
2018-02-07 00:25:53 -0500

    make btb logging name consistent with rest of library

--- btb/__init__.py
+++ btb/__init__.py
@@ -4,4 +4,5 @@ from .hyper_parameter import *
 # configure logging for the library with a null handler (nothing is printed by
 # default). See http://docs.python-guide.org/en/latest/writing/logging/
 import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+logging.getLogger('btb').addHandler(logging.NullHandler())
