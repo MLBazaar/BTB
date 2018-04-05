681debb507c4fe8b573e706009266872615ae234
Carles Sala <c.sala.stq@gmail.com>
2018-04-05 10:14:01 +0200

    Fix import order

--- btb/selection/__init__.py
+++ btb/selection/__init__.py
@@ -1,9 +1,9 @@
+from btb.selection.selector import Selector  # noqa I001
+from btb.selection.ucb1 import UCB1  # noqa I001
 from btb.selection.best import BestKReward, BestKVelocity
 from btb.selection.hierarchical import HierarchicalByAlgorithm
 from btb.selection.pure import PureBestKVelocity
 from btb.selection.recent import RecentKReward, RecentKVelocity
-from btb.selection.selector import Selector
-from btb.selection.ucb1 import UCB1
 from btb.selection.uniform import Uniform
 
 __all__ = (
