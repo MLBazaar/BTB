0b795f1ff3587500611ae72693735e6bae2f8e99
Carles Sala <c.sala.stq@gmail.com>
2018-04-11 22:26:43 +0200

    Match HDI-Poject Cookiecutter template

---
+++ AUTHORS.rst
@@ -0,0 +1,18 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Kalyan Veeramachaneni <kalyan@csail.mit.edu>
+
+Contributors
+------------
+
+* Laura Gustafson <lgustaf@mit.edu>
+* Bennett Cyphers <bennettcyphers@gmail.com>
+* Carles Sala <carles@pythiac.com>
+* Micah Smith <micahs@mit.edu>
+* Alfredo Cuesta
+* Kalyan Veeramachaneni <kalyan@csail.mit.edu>
--- btb/__init__.py
+++ btb/__init__.py
@@ -1,13 +1,20 @@
+# -*- coding: utf-8 -*-
 # configurom btre logging for the library with a null handler (nothing is
 # printed by default). See
 # http://docs.pthon-guide.org/en/latest/writing/logging/
+
+"""Top-level package for BTB."""
+
+__author__ = 'The Human Data Interaction Project'
+__email__ = 'kalyan@csail.mit.edu'
+__version__ = '0.1.0'
+
 import logging
 
-from btb.hyper_parameter import (CLASS_GENERATOR, BoolCatHyperParameter, CatHyperParameter,
-                                 FloatCatHyperParameter, FloatExpHyperParameter,
-                                 FloatHyperParameter, HyperParameter, IntCatHyperParameter,
-                                 IntExpHyperParameter, IntHyperParameter, ParamTypes,
-                                 StringCatHyperParameter)
+from btb.hyper_parameter import (
+    CLASS_GENERATOR, BoolCatHyperParameter, CatHyperParameter, FloatCatHyperParameter,
+    FloatExpHyperParameter, FloatHyperParameter, HyperParameter, IntCatHyperParameter,
+    IntExpHyperParameter, IntHyperParameter, ParamTypes, StringCatHyperParameter)
 
 __all__ = (
     CLASS_GENERATOR, BoolCatHyperParameter, CatHyperParameter,
