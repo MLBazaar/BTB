58bc5f45115b3689199db347899e40e3918f9dc1
Micah Smith <micahjsmith@gmail.com>
2018-04-12 19:53:03 -0400

    Add __version__ to package

--- btb/__init__.py
+++ btb/__init__.py
@@ -1,6 +1,10 @@
 # configurom btre logging for the library with a null handler (nothing is
 # printed by default). See
 # http://docs.pthon-guide.org/en/latest/writing/logging/
+
+
+__version__ = '0.2.0'
+
 import logging
 
 from btb.hyper_parameter import (
