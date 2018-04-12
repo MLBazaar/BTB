bdb8788fee9ba617991c751125f07877d3f477a2
Micah Smith <micahjsmith@gmail.com>
2018-04-12 18:12:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,69 +1,57 @@
-"""
-Stripped down from the example at:
-https://github.com/pypa/sampleproject
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import os
+"""The setup script."""
 
-# Always prefer setuptools over distutils
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'future>=0.16.0',
+    'numpy>=1.14.2',
+    'scikit-learn>=0.19.1',
+    'scipy>=1.0.1'
+    'six',
+]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
 
 setup(
-    name='btb',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
-
-    description='A framework for Bayesian hyperparameter selection and tuning',
-
-    # The project's main homepage.
-    url='https://github.com/HDI-project/BTB',
-
-    # Choose your license
-    license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author="MIT Data To AI Lab",
+    author_email='dai-lab-btb@mit.edu',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-
-    # What does your project relate to?
+    description='A framework for Bayesian hyperparameter selection and tuning',
+    install_requires=requirements,
+    license='MIT license',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
     keywords='machine learning hyperparameters tuning classification',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'future==0.16.0',
-        'numpy==1.14.2',
-        'scikit-learn==0.19.1',
-        'scipy==1.0.1'
-    ]
+    name='btb',
+    packages=find_packages(include=['btb', 'btb.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/HDI-Project/btb',
+    version='0.2.0',
+    zip_safe=False,
 )
