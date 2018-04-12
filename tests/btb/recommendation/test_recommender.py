0c0a27379e1c89e4fc81eed4741b6e81edcf689f
Laura Gustafson <lgustaf@mit.edu>
2018-04-12 12:25:57 -0400

    split up unit tests

--- tests/btb/recommendation/test_recommender.py
+++ tests/btb/recommendation/test_recommender.py
@@ -32,8 +32,8 @@ class TestBaseRecommender(TestCase):
         ])
 
     @patch('btb.recommendation.recommender.NMF')
-    def test___init__(self, NMF_mock):
-        NMF_mock().fit_transform.return_value = self.dpp_ranked
+    def test___init__(self, nmf_mock):
+        nmf_mock().fit_transform.return_value = self.dpp_ranked
         # Run
         recommender = Recommender(self.dpp_matrix, self.n_components)
         np.testing.assert_array_equal(recommender.dpp_matrix, self.dpp_matrix)
@@ -44,24 +44,22 @@ class TestBaseRecommender(TestCase):
         assert recommender.dpp_vector.shape[0] == self.dpp_matrix.shape[1]
 
     @patch('btb.recommendation.recommender.NMF')
-    def test_fit(self, NMF_mock):
-        NMF_mock().fit_transform.return_value = self.dpp_ranked
+    def test_fit(self, nmf_mock):
+        nmf_mock().fit_transform.return_value = self.dpp_ranked
         n_components = 3
         X = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         # Run
         recommender = Recommender(self.dpp_matrix, n_components)
         for i in range(self.dpp_ranked.shape[0]):
-            NMF_mock().transform.return_value = self.dpp_ranked[i, :]
+            nmf_mock().transform.return_value = self.dpp_ranked[i, :]
             recommender.fit(X)
             np.testing.assert_array_equal(
                 recommender.matching_dataset,
                 self.dpp_matrix[i],
             )
 
-    def test__get_candidates(self):
-        # Run
+    def test__get_candidates_all(self):
         recommender = Recommender(self.dpp_matrix, self.n_components)
-
         X = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         recommender.X = X
         candidates = recommender._get_candidates()
@@ -73,6 +71,8 @@ class TestBaseRecommender(TestCase):
             expected,
         )
 
+    def test__get_candidates_some(self):
+        recommender = Recommender(self.dpp_matrix, self.n_components)
         X = np.array([0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0])
         recommender.X = X
         expected = np.array([0, 1, 2, 4, 6, 8, 10, 11, 12, 14, 15])
@@ -82,33 +82,37 @@ class TestBaseRecommender(TestCase):
             expected,
         )
 
+    def test__get_candidates_none(self):
+        recommender = Recommender(self.dpp_matrix, self.n_components)
         X = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
         recommender.X = X
         candidates = recommender._get_candidates()
         expected = None
         self.assertEqual(expected, candidates)
 
-    def test__acquire(self):
-        # Run
+    def test__acquire_last(self):
         recommender = Recommender(self.dpp_matrix, self.n_components)
-
         predictions = np.array([1, 3, 5, 7])
         acquired = recommender._acquire(predictions)
         expected = 3
         self.assertEqual(acquired, expected)
 
+    def test__acquire_middle(self):
+        recommender = Recommender(self.dpp_matrix, self.n_components)
         predictions = np.array([9, 10, 5, 7])
         acquired = recommender._acquire(predictions)
         expected = 1
         self.assertEqual(acquired, expected)
 
+    def test__acquire_multiple(self):
+        recommender = Recommender(self.dpp_matrix, self.n_components)
         predictions = np.array([1, 9, 9, 7])
         acquired = recommender._acquire(predictions)
         expected_1 = 2
         expected_2 = 1
         self.assertTrue(acquired == expected_1 or acquired == expected_2)
 
-    def test_predict(self):
+    def test_predict_one(self):
         # Run
         recommender = Recommender(self.dpp_matrix, self.n_components)
         # matching row is [1, 0, 0, 0, 0, 0, 0, 3, 0, 4, 6, 2, 5, 0, 8, 0]
@@ -117,21 +121,30 @@ class TestBaseRecommender(TestCase):
         indicies = [0]
         predictions = recommender.predict(indicies)
         expected = [1]
-        print("predictions", predictions)
         np.testing.assert_array_equal(predictions, expected)
 
+    def test_predict_all_matching(self):
+        # Run
+        recommender = Recommender(self.dpp_matrix, self.n_components)
+        # matching row is [1, 0, 0, 0, 0, 0, 0, 3, 0, 4, 6, 2, 5, 0, 8, 0]
+        recommender.matching_dataset = self.dpp_matrix[0]
         indicies = [1, 2, 3, 4, 5]
         predictions = recommender.predict(indicies)
         expected = [1, 1, 1, 1, 1]
         np.testing.assert_array_equal(predictions, expected)
 
+    def test_predict_multiple_rankings(self):
+        # Run
+        recommender = Recommender(self.dpp_matrix, self.n_components)
+        # matching row is [1, 0, 0, 0, 0, 0, 0, 3, 0, 4, 6, 2, 5, 0, 8, 0]
+        recommender.matching_dataset = self.dpp_matrix[0]
         indicies = [0, 1, 14, 13]
         predictions = recommender.predict(indicies)
         expected = [2, 1, 3, 1]
         np.testing.assert_array_equal(predictions, expected)
 
     @patch('btb.recommendation.recommender.Recommender.fit')
-    def test_add(self, fit_mock):
+    def test_add_none(self, fit_mock):
         recommender = Recommender(self.dpp_matrix, self.n_components)
         recommender.add({})
         expected_x = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
@@ -142,6 +155,8 @@ class TestBaseRecommender(TestCase):
         )
         fit_mock.assert_called_once()
 
+    @patch('btb.recommendation.recommender.Recommender.fit')
+    def test_add_once(self, fit_mock):
         recommender = Recommender(self.dpp_matrix, self.n_components)
         recommender.add({1: 2, 3: 4, 5: 1})
         expected_x_1 = np.array(
@@ -152,7 +167,20 @@ class TestBaseRecommender(TestCase):
             fit_mock.call_args[0][0],
             expected_x_1.reshape(1, -1)
         )
+        fit_mock.assert_called_once()
 
+    @patch('btb.recommendation.recommender.Recommender.fit')
+    def test_add_twice(self, fit_mock):
+        recommender = Recommender(self.dpp_matrix, self.n_components)
+        recommender.add({1: 2, 3: 4, 5: 1})
+        expected_x_1 = np.array(
+            [0, 2, 0, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        )
+        np.testing.assert_array_equal(recommender.dpp_vector, expected_x_1)
+        np.testing.assert_array_equal(
+            fit_mock.call_args[0][0],
+            expected_x_1.reshape(1, -1)
+        )
         recommender.add({1: 1, 6: 3, 9: 4})
         expected_x_2 = np.array(
             [0, 1, 0, 4, 0, 1, 3, 0, 0, 4, 0, 0, 0, 0, 0, 0]
@@ -162,11 +190,11 @@ class TestBaseRecommender(TestCase):
             fit_mock.call_args[0][0],
             expected_x_2.reshape(1, -1)
         )
-
+        self.assertEqual(fit_mock.call_count, 2)
 
     @patch('btb.recommendation.recommender.Recommender._get_candidates')
     @patch('btb.recommendation.recommender.Recommender.predict')
-    def test_propse_done(self, predict_mock, get_candidates_mock):
+    def test_propose_done(self, predict_mock, get_candidates_mock):
         # Set-up
         recommender = Recommender(self.dpp_matrix, self.n_components)
 
@@ -181,7 +209,7 @@ class TestBaseRecommender(TestCase):
 
     @patch('btb.recommendation.recommender.Recommender._get_candidates')
     @patch('btb.recommendation.recommender.Recommender.predict')
-    def test_propse_n_eq_1(self, predict_mock, get_candidates_mock):
+    def test_propose_once(self, predict_mock, get_candidates_mock):
         """n == 1"""
         # Set-up
         recommender = Recommender(self.dpp_matrix, self.n_components)
