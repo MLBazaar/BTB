b3fe5267c40258a59e6fd530d7c1536719734bcc
Carles Sala <c.sala.stq@gmail.com>
2018-05-31 14:43:35 -0400

    Remove unnecessary things from __init__.py the public API

--- btb/recommendation/__init__.py
+++ btb/recommendation/__init__.py
@@ -1,9 +1,4 @@
 from btb.recommendation.matrix_factorization import MFRecommender
-from btb.recommendation.recommender import BaseRecommender
 from btb.recommendation.uniform import UniformRecommender
 
-__all__ = (
-    'BaseRecommender',
-    'MFRecommender',
-    'UniformRecommender'
-)
+__all__ = ('BaseRecommender', 'MFRecommender', 'UniformRecommender')
