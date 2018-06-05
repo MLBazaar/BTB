32611f84a25e2ae1c7b69a86b56f440720f84c64
Carles Sala <c.sala.stq@gmail.com>
2018-06-05 15:55:09 -0400

    Fix versions for 0.2.1 release

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/BTB',
-    version='0.3.0-dev',
+    version='0.2.1-dev',
     zip_safe=False,
 )
