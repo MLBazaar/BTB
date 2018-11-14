cafaaeb3bf70fb04c5365c6bfd7b79210a613583
Micah Smith <micahjsmith@gmail.com>
2018-11-13 22:16:04 -0500

    Bump version: 0.2.3-dev → 0.2.3

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/BTB',
-    version='0.2.3-dev',
+    version='0.2.3',
     zip_safe=False,
 )
