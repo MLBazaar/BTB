daf39f603c59f56bc59adac252cec141d62e3df8
Micah Smith <micahjsmith@gmail.com>
2018-11-28 17:58:00 -0500

    Add asarray2d util function

--- btb/util.py
+++ btb/util.py
@@ -1,5 +1,7 @@
 import random
 
+import numpy as np
+
 
 def shuffle(iterable):
     iterable = list(iterable)
@@ -7,3 +9,11 @@ def shuffle(iterable):
     random.shuffle(inds)
     for i in inds:
         yield iterable[i]
+
+
+def asarray2d(arr):
+    """Cast to 2d array"""
+    arr = np.asarray(arr)
+    if arr.ndim == 1:
+        arr = arr.reshape(-1, 1)
+    return arr
