006149401a3bf48b4dfc529934e11775c5b17401
Carles Sala <carles@pythiac.com>
2019-03-15 18:52:34 +0100

    Bump version: 0.2.5-dev → 0.2.5

--- setup.py
+++ setup.py
@@ -98,6 +98,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/BTB',
-    version='0.2.5-dev',
+    version='0.2.5',
     zip_safe=False,
 )
