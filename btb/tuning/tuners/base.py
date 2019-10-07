495414d239f35399135bd454fdb2c491fef3d6a4
Plamen Valentinov <pvkdeveloper@gmx.com>
2019-10-07 15:33:11 +0200

    Update docstrings

--- btb/tuning/tuners/base.py
+++ btb/tuning/tuners/base.py
@@ -17,9 +17,9 @@ class BaseTuner:
             Instance of a tunable class containing hyperparameters to be tuned.
         trials (numpy.ndarray):
             A ``numpy.ndarray`` with shape ``(n, self.tunable.K)`` where ``n`` is the number of
-            trials stored.
+            trials recorded.
         scores (numpy.ndarray):
-            A ``numpy.ndarray`` with shape ``(n, 1)`` where ``n`` is the number of scores stored.
+            A ``numpy.ndarray`` with shape ``(n, 1)`` where ``n`` is the number of scores recorded.
 
     Args:
         tunable (btb.tuning.tunable.Tunable):
@@ -32,6 +32,21 @@ class BaseTuner:
         self.scores = np.empty((0, 1), dtype=np.float)
 
     def _check_proposals(self, num_proposals):
+        """Validate ``num_proposals`` with ``self.tunable.SC`` and ``self.trials``.
+
+        Raises:
+            ValueError:
+                A ``ValueError`` exception is being produced if the amount of requested proposals
+                is bigger than the possible combinations and ``allow_duplicates`` is ``False``.
+
+            ValueError:
+                A ``ValueError`` exception is being produced if the unique amount of recorded
+                trials is the same as the amount of combinations available for ``self.tunable``.
+
+            ValueError:
+                A ``ValueError`` exception is being produced if the unique amount of recorded
+                trials is the same as the amount of combinations available for ``self.tunable``.
+        """
         if num_proposals > self.tunable.SC:
             raise ValueError(
                 'The number of proposals requested is bigger than the combinations: {} of the'
@@ -58,16 +73,15 @@ class BaseTuner:
     def _sample(self, num_proposals, allow_duplicates):
         """Generate a ``numpy.ndarray`` of valid proposals.
 
-        Generates ``num_proposals`` of valid proposals by generating ``proposals`` until
-        ``len(valid_proposals) == num_proposals`` or ``len(valid_proposals) == self.tunable.SC``
-        being those ``proposals`` different from the ones that have been recorded.
+        Generates ``num_proposals`` of valid combinations by generating ``proposals`` until
+        ``len(valid_proposals) == num_proposals`` different from the ones that have been recorded.
 
         Args:
             num_proposals (int):
                 Amount of proposals to generate.
             allow_duplicates (bool):
                 If it's ``False``, the tuner will propose trials that are not recorded. Otherwise
-                will generate trials that can be repeated.
+                will generate trials that may have been already recorded.
 
         Returns:
             numpy.ndarray:
@@ -105,7 +119,7 @@ class BaseTuner:
         Returns:
             numpy.ndarray:
                 It returns ``numpy.ndarray`` with shape
-                ``(num_proposals, len(tunable.hyperparameters)``.
+                ``(num_proposals, len(self.tunable.hyperparameters)``.
         """
         pass
 
@@ -116,7 +130,7 @@ class BaseTuner:
         ``False`` and raise an exception in case there is any missmatch between ``num_proposals``,
         ``self.trials`` and ``self.tunable.SC``.
         Call the implemented ``_propose`` method and convert the returned data in to hyperparameter
-        space.
+        space values.
 
         Args:
             num_proposals (int):
@@ -143,6 +157,27 @@ class BaseTuner:
             ValueError:
                 A ``ValueError`` exception is being produced if the unique amount of recorded
                 trials is the same as the amount of combinations available for ``self.tunable``.
+
+        Example:
+            The example below shows simple usage case where an ``UniformTuner`` is being imported,
+            instantiated with a ``tunable`` object and it's method propose is being called
+            three times, first with a single proposal, a second with two proposals forcing them to
+            be different and once where the values can be repeated.
+
+            >>> from btb.tuning.tunable import Tunable
+            >>> from btb.tuning.hyperparams import BooleanHyperParam
+            >>> from btb.tuning.hyperparams import CategoricalHyperParam
+            >>> from btb.tuning.tuners import UniformTuner
+            >>> bhp = BooleanHyperParam()
+            >>> chp = CategoricalHyperParam(['cat', 'dog'])
+            >>> tunable = Tunable({'bhp': bhp, 'chp': chp})
+            >>> tuner = UniformTuner(tunable)
+            >>> tuner.propose(1)
+            {'bhp': True, 'chp': 'dog'}
+            >>> tuner.propose(2)
+            [{'bhp': True, 'chp': 'cat'}, {'bhp': True, 'chp': 'dog'}]
+            >>> tuner.propose(2, allow_duplicates=True)
+            [{'bhp': False, 'chp': 'dog'}, {'bhp': False, 'chp': 'dog'}]
         """
 
         if not allow_duplicates:
@@ -161,12 +196,39 @@ class BaseTuner:
         """Record one or more ``trials`` with the associated ``scores``.
 
         Records one or more ``trials`` with the associated ``scores`` to it. The amount of trials
-        must be equal to the amount of scores recived (and vice versa).
+        must be equal to the amount of scores recived and vice versa.
+
+        Args:
+            trials (pandas.DataFrame, pandas.Series, dict, list(dict), 2D array-like):
+                Values of shape ``(n, len(self.tunable.hyperparameters))`` or dict with keys that
+                are ``self.tunable.names``.
+
+            scores (single value or array-like):
+                A single value or array-like of values representing the score achieved with the
+                trials.
 
         Raises:
             ValueError:
                 A ``ValueError`` exception is being produced if ``len(trials)`` is not equal to
                 ``len(scores)``.
+
+        Example:
+            The example below shows simple usage case where an ``UniformTuner`` is being imported,
+            instantiated with a ``tunable`` object and it's method record is being called two times
+            with valid trials and scores.
+
+            >>> from btb.tuning.tunable import Tunable
+            >>> from btb.tuning.hyperparams import BooleanHyperParam
+            >>> from btb.tuning.hyperparams import CategoricalHyperParam
+            >>> from btb.tuning.tuners import UniformTuner
+            >>> bhp = BooleanHyperParam()
+            >>> chp = CategoricalHyperParam(['cat', 'dog'])
+            >>> tunable = Tunable({'bhp': bhp, 'chp': chp})
+            >>> tuner = UniformTuner(tunable)
+            >>> tuner.record({'bhp': True, 'chp': 'cat'}, 0.8)
+            >>> trials = [{'bhp': False, 'chp': 'cat'}, {'bhp': True, 'chp': 'dog'}]
+            >>> scores = [0.8, 0.1]
+            >>> tuner.record(trials, scores)
         """
 
         trials = self.tunable.transform(trials)
--- btb/tuning/tuners/uniform.py
+++ btb/tuning/tuners/uniform.py
@@ -8,5 +8,18 @@ from btb.tuning.tuners.base import BaseTuner
 class UniformTuner(BaseTuner):
 
     def _propose(self, num_proposals, allow_duplicates):
-        """Returns a ``num_proposals`` number of samples."""
+        """Generate ``num_proposals`` number of candidates.
+
+        Args:
+            num_proposals (int):
+                Number of candidates to create.
+            allow_duplicates (bool):
+                If it's ``False``, the tuner will propose trials that are not recorded. Otherwise
+                will generate trials that can be repeated.
+
+        Returns:
+            numpy.ndarray:
+                It returns ``numpy.ndarray`` with shape
+                ``(num_proposals, len(self.tunable.hyperparameters)``.
+        """
         return self._sample(num_proposals, allow_duplicates)
