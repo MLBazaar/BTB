ad2f8bfd817919a5a735830acb3fb25be27f6794
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2019-10-14 22:20:21 +0200

    Import all tuners.

--- btb/tuning/tuners/__init__.py
+++ btb/tuning/tuners/__init__.py
@@ -2,6 +2,7 @@
 
 """Package where all the available tuners are imported."""
 
+from btb.tuning.tuners.gaussian_process import GPEiTuner, GPTuner
 from btb.tuning.tuners.uniform import UniformTuner
 
-__all__ = ('UniformTuner', )
+__all__ = ('GPEiTuner', 'GPTuner', 'UniformTuner', )
