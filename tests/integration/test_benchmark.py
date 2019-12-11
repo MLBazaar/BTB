91a6a8d078d4d7cb351ad5d14ab2958d3fc1f1dd
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2019-12-11 23:02:32 +0100

    Add integration test.

---
+++ tests/integration/test_benchmark.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from btb.benchmark import benchmark
+from btb.benchmark.challenges import Rosenbrock
+from btb.benchmark.tuners.btb import make_tuning_function
+from btb.tuning import GPTuner, Tunable
+
+
+def test_benchmark_rosenbrock():
+
+    candidate = make_tuning_function(GPTuner)
+    benchmark(candidate, challenges=Rosenbrock(), iterations=1)
