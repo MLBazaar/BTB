25c1b535461f6e0c1aead7480bafabbcbcbdecc6
Plamen Valentinov <pvkdeveloper@gmx.com>
2020-01-21 18:02:14 +0100

    Bump version: 0.3.5 → 0.3.6.dev0

--- btb/__init__.py
+++ btb/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.3.5'
+__version__ = '0.3.6.dev0'
--- setup.py
+++ setup.py
@@ -110,6 +110,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/BTB',
-    version='0.3.5',
+    version='0.3.6.dev0',
     zip_safe=False,
 )
