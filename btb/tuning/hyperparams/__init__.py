22f50c8dc42a36d7ad89ccc57f20a02168713806
Plamen Valentinov <pvkdeveloper@gmx.com>
2020-02-06 16:13:36 +0100

    Implemented function that instantiates hyperparameters from a dictionary.

--- btb/tuning/hyperparams/__init__.py
+++ btb/tuning/hyperparams/__init__.py
@@ -7,3 +7,34 @@ from btb.tuning.hyperparams.categorical import CategoricalHyperParam
 from btb.tuning.hyperparams.numerical import FloatHyperParam, IntHyperParam
 
 __all__ = ('BooleanHyperParam', 'CategoricalHyperParam', 'FloatHyperParam', 'IntHyperParam')
+
+
+def instantiate_hyperparam_from_dict(dict_hyperparam):
+    """Instantiate a hyperparameter from a dictionary"""
+
+    if not isinstance(dict_hyperparam, dict):
+        raise TypeError('Hyperparams must be a dictionary.')
+
+    hp_type = dict_hyperparam['type']
+    hp_default = dict_hyperparam.get('default')
+
+    if hp_type == 'int':
+        hp_range = dict_hyperparam.get('range') or dict_hyperparam.get('values')
+        hp_min = min(hp_range) if hp_range else None
+        hp_max = max(hp_range) if hp_range else None
+        hp_instance = IntHyperParam(min=hp_min, max=hp_max, default=hp_default)
+
+    elif hp_type == 'float':
+        hp_range = dict_hyperparam.get('range') or dict_hyperparam.get('values')
+        hp_min = min(hp_range)
+        hp_max = max(hp_range)
+        hp_instance = FloatHyperParam(min=hp_min, max=hp_max, default=hp_default)
+
+    elif hp_type == 'bool':
+        hp_instance = BooleanHyperParam(default=hp_default)
+
+    elif hp_type == 'str':
+        hp_choices = dict_hyperparam.get('range') or dict_hyperparam.get('values')
+        hp_instance = CategoricalHyperParam(choices=hp_choices, default=hp_default)
+
+    return hp_instance
