b0bc4774081f914afe637666f3417c043ab5aad5
Plamen Valentinov <pvkdeveloper@gmx.com>
2020-02-12 12:00:08 +0100

    Add tuning assertions.

--- tests/integration/test_tuning.py
+++ tests/integration/test_tuning.py
@@ -22,3 +22,30 @@ def test_tuning():
         tuner.record(proposed, random.random())
 
     # TODO add asserts
+    assert len(tuner.trials) == 10
+    assert len(tuner._trials_set) == 10
+    assert len(tuner.raw_scores) == 10
+    assert len(tuner.scores) == 10
+    assert all(tuner.raw_scores == tuner.scores)
+
+
+def test_tuning_minimize():
+    hyperparams = {
+        'bhp': BooleanHyperParam(default=False),
+        'chp': CategoricalHyperParam(choices=['a', 'b', None], default=None),
+        'fhp': FloatHyperParam(min=0.1, max=1.0, default=0.5),
+        'ihp': IntHyperParam(min=-1, max=1)
+    }
+    tunable = Tunable(hyperparams)
+    tuner = GPTuner(tunable, maximize=False)
+
+    for _ in range(10):
+        proposed = tuner.propose(1)
+        tuner.record(proposed, random.random())
+
+    # TODO add asserts
+    assert len(tuner.trials) == 10
+    assert len(tuner._trials_set) == 10
+    assert len(tuner.raw_scores) == 10
+    assert len(tuner.scores) == 10
+    assert all(-tuner.raw_scores == tuner.scores)
