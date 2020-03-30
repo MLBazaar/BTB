6d2ae7846653b052eaca44ccbf60f055a2dec49a
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-03-30 16:50:03 +0200

    Refactor names / inspect.

--- btb/benchmark/__init__.py
+++ btb/benchmark/__init__.py
@@ -1,67 +1,69 @@
 # -*- coding: utf-8 -*-
-import inspect
 import logging
+from datetime import datetime
 
 import dask
 import pandas as pd
 
-from btb.benchmark.challenges import MATH_CHALLENGES, ML_CHALLENGES
+from btb.benchmark.challenges import MATH_CHALLENGES, Challenge
 
-DEFAULT_CHALLENGES = MATH_CHALLENGES + ML_CHALLENGES
 LOGGER = logging.getLogger(__name__)
 
 
 @dask.delayed
-def _evaluate_candidate(name, candidate, challenge, iterations):
+def _evaluate_tuner(name, tuner, challenge, iterations):
     tunable_hyperparameters = challenge.get_tunable_hyperparameters()
-    LOGGER.info('Evaluating candidate %s on challenge %s for %s iterations',
+    LOGGER.info('Evaluating tuner %s on challenge %s for %s iterations',
                 name, challenge, iterations)
     try:
-        score = candidate(challenge.evaluate, tunable_hyperparameters, iterations)
+        start = datetime.utcnow()
+        score = tuner(challenge.evaluate, tunable_hyperparameters, iterations)
         result = {
             'challenge': str(challenge),
-            'candidate': name,
+            'tuner': name,
             'score': score,
+            'elapsed': datetime.utcnow() - start
         }
 
     except Exception as ex:
         LOGGER.warn(
-            'Could not score candidate %s with challenge %s, error: %s', name, challenge, ex)
+            'Could not score tuner %s with challenge %s, error: %s', name, challenge, ex)
         result = {
             'challenge': str(challenge),
-            'candidate': name,
+            'tuner': name,
             'score': None,
+            'elapsed': datetime.utcnow() - start
         }
 
     return result
 
 
-def evaluate_candidate(name, candidate, challenges, iterations):
-    candidate_results = []
+def evaluate_tuner(name, tuner, challenges, iterations):
+    tuner_results = []
     for challenge in challenges:
         try:
-            if inspect.isclass(challenge):
+            if issubclass(challenge, Challenge):
                 challenge = challenge()
 
-            result = _evaluate_candidate(name, candidate, challenge, iterations)
+            result = _evaluate_tuner(name, tuner, challenge, iterations)
 
         except Exception as ex:
             LOGGER.warn(
-                'Could not score candidate %s with challenge %s, error: %s', name, challenge, ex)
+                'Could not score tuner %s with challenge %s, error: %s', name, challenge, ex)
 
-        candidate_results.append(result)
+        tuner_results.append(result)
 
-    return candidate_results
+    return tuner_results
 
 
-def benchmark(candidates, challenges=None, iterations=1000):
+def benchmark(tuners, challenges=None, iterations=1000, verbose_dataframe=False):
     """Benchmark function.
 
     This benchmark function iterates over a collection of ``challenges`` and executes a
     ``tuner_function`` for each one of the ``challenges`` for a given amount of iterations.
 
     Args:
-        candidates (callable, list, tuple or dict):
+        tuners (callable, list, tuple or dict):
             Python callable function, list of callable functions, tuple with callable functions or
             dictionary with the ``name`` of the function as ``key`` and the callable function that
             returns the best score for a given ``scorer``. This function must have three arguments:
@@ -87,30 +89,34 @@ def benchmark(candidates, challenges=None, iterations=1000):
             returned.
     """
     if challenges is None:
-        challenges = DEFAULT_CHALLENGES
+        challenges = MATH_CHALLENGES
     elif not isinstance(challenges, list):
         challenges = [challenges]
 
-    if callable(candidates):
-        candidates = {candidates.__name__: candidates}
-    elif isinstance(candidates, (list, tuple)):
-        candidates = {candidate.__name__: candidate for candidate in candidates}
-    elif not isinstance(candidates, dict):
-        raise TypeError('Candidates can only be a callable, list or dict.')
+    if callable(tuners):
+        tuners = {tuners.__name__: tuners}
+    elif isinstance(tuners, (list, tuple)):
+        tuners = {tuner.__name__: tuner for tuner in tuners}
+    elif not isinstance(tuners, dict):
+        raise TypeError('tuners can only be a callable, list or dict.')
 
     results = []
 
-    for name, candidate in candidates.items():
-        LOGGER.info('Evaluating candidate %s', name)
+    for name, tuner in tuners.items():
+        LOGGER.info('Evaluating tuner %s', name)
 
-        result = evaluate_candidate(name, candidate, challenges, iterations)
+        result = evaluate_tuner(name, tuner, challenges, iterations)
 
         results.extend(result)
 
     results = [result for result in dask.compute(*results)]
 
     df = pd.DataFrame.from_records(results)
-    df = df.pivot(index='challenge', columns='candidate', values='score')
+
+    if verbose_dataframe:
+        return df
+
+    df = df.pivot(index='challenge', columns='tuner', values='score')
 
     del df.columns.name
     del df.index.name
