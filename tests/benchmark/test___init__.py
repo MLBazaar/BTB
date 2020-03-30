b59bd1d3887942d97c6594a1b7581992918f4e64
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-03-30 16:52:15 +0200

    Update tests to new code.

--- tests/benchmark/test___init__.py
+++ tests/benchmark/test___init__.py
@@ -10,31 +10,31 @@ from btb.benchmark import benchmark
 
 class TestBenchmark(TestCase):
 
-    @patch('btb.benchmark.evaluate_candidate')
-    def test_benchmark_challenges_callable(self, mock_evaluate_candidate):
+    @patch('btb.benchmark.evaluate_tuner')
+    def test_benchmark_challenges_callable(self, mock_evaluate_tuner):
         # setup
-        mock_evaluate_candidate.return_value = [{
+        mock_evaluate_tuner.return_value = [{
             'challenge': 'test_challenge',
-            'candidate': 'test_candidate',
+            'tuner': 'test_tuner',
             'score': 1.0
         }]
 
-        candidate = MagicMock(__name__='test_candidate')
+        tuner = MagicMock(__name__='test_tuner')
         challenge = MagicMock()
 
         # run
-        result = benchmark(candidate, challenges=challenge)
+        result = benchmark(tuner, challenges=challenge)
 
         # assert
         expected_result = pd.DataFrame({
-            'test_candidate': [1.0],
+            'test_tuner': [1.0],
         })
 
         expected_result.index = ['test_challenge']
 
-        mock_evaluate_candidate.assert_called_once_with(
-            'test_candidate',
-            candidate,
+        mock_evaluate_tuner.assert_called_once_with(
+            'test_tuner',
+            tuner,
             [challenge],
             1000
         )
@@ -44,26 +44,26 @@ class TestBenchmark(TestCase):
             expected_result.sort_index(axis=1),
         )
 
-    @patch('btb.benchmark.evaluate_candidate')
-    def test_benchmark_challenges_tuple(self, mock_evaluate_candidate):
+    @patch('btb.benchmark.evaluate_tuner')
+    def test_benchmark_challenges_tuple(self, mock_evaluate_tuner):
         # setup
-        mock_evaluate_candidate.side_effect = [
-            [{'challenge': 'test_challenge', 'candidate': 'candidate_a', 'score': 1.0}],
-            [{'challenge': 'test_challenge', 'candidate': 'candidate_b', 'score': 1.0}],
+        mock_evaluate_tuner.side_effect = [
+            [{'challenge': 'test_challenge', 'tuner': 'tuner_a', 'score': 1.0}],
+            [{'challenge': 'test_challenge', 'tuner': 'tuner_b', 'score': 1.0}],
         ]
 
-        candidate_a = MagicMock(return_value=0.1, __name__='candidate_a')
-        candidate_b = MagicMock(return_value=0.1, __name__='candidate_b')
-        candidates = (candidate_a, candidate_b)
+        tuner_a = MagicMock(return_value=0.1, __name__='tuner_a')
+        tuner_b = MagicMock(return_value=0.1, __name__='tuner_b')
+        tuners = (tuner_a, tuner_b)
         challenge = MagicMock()
 
         # run
-        result = benchmark(candidates, challenges=challenge)
+        result = benchmark(tuners, challenges=challenge)
 
         # assert
         expected_result = pd.DataFrame({
-            'candidate_a': [1.0],
-            'candidate_b': [1.0],
+            'tuner_a': [1.0],
+            'tuner_b': [1.0],
         })
 
         expected_result.index = ['test_challenge']
@@ -73,7 +73,7 @@ class TestBenchmark(TestCase):
             expected_result.sort_index(axis=1),
         )
 
-    def test_benchmark_candidates_not_dict_not_callable(self):
+    def test_benchmark_tuners_not_dict_not_callable(self):
         # setup
         with self.assertRaises(TypeError):
             benchmark(1)
