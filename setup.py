a2989429a8fa9f63c26cb6a5d068771416074bd5
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-04-15 13:39:38 +0200

    Bump version: 0.3.7.dev0 → 0.3.7

--- setup.py
+++ setup.py
@@ -113,6 +113,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/BTB',
-    version='0.3.7.dev0',
+    version='0.3.7',
     zip_safe=False,
 )
