3038495a818f50b2afe20c003c4c3ab728773757
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-05-08 21:07:48 +0200

    Bump version: 0.3.8 → 0.3.9.dev0

--- benchmark/btb_benchmark/__init__.py
+++ benchmark/btb_benchmark/__init__.py
@@ -4,5 +4,5 @@ from btb_benchmark.main import run_benchmark
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.3.8'
+__version__ = '0.3.9.dev0'
 __all__ = ('run_benchmark', )
