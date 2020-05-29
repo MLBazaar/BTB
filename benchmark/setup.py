c43919113c6d9a16313f9a06c85e125e03309108
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-05-29 23:40:03 +0200

    Bump version: 0.3.10.dev0 → 0.3.10

--- benchmark/setup.py
+++ benchmark/setup.py
@@ -131,6 +131,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/BTB',
-    version='0.3.10.dev0',
+    version='0.3.10',
     zip_safe=False,
 )
--- btb/__init__.py
+++ btb/__init__.py
@@ -6,5 +6,5 @@ from btb.session import BTBSession
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.3.10.dev0'
+__version__ = '0.3.10'
 __all__ = ('BTBSession', )
