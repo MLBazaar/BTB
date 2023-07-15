2245be0e8e84b81d1db4191dd651b383efb2b898
Gaurav Sheni <gvsheni@gmail.com>
2023-07-15 15:19:45 -0400

    cleanup more

--- unpacked_sdist/benchmark/btb_benchmark/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from btb_benchmark.main import run_benchmark
-
-__author__ = """MIT Data To AI Lab"""
-__email__ = 'dailabmit@gmail.com'
-__version__ = '0.4.1.dev0'
-__all__ = ('run_benchmark', )
--- unpacked_sdist/tests/test_version.py
+++
@@ -1,4 +0,0 @@
-from baytune import __version__
-
-def test_version():
-    assert __version__ == "0.4.1"
